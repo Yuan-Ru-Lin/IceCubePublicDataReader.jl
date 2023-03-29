50eef383ee55e92d4df28796e4a6c30291d89818
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2023-03-30 01:14:25 +0800

    Get ready for publication

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,17 @@
 # Introduction
 
-The package implements I/O for IceCube Public Data Set whose spec is illustrated in https://icecube.umd.edu/PublicData/.
+The package implements I/O for IceCube Public Data whose event bytestream is illustrated in [this page](https://icecube.umd.edu/PublicData/):
+
+> 1. 4 bytes - runID(unsigned int)
+> 2. 4 bytes - year(unsigned int)
+> 3. 8 bytes - startTime (long) Number of tenths of nanoseconds since the beginning of the year.
+> 4. 8 bytes - eventLength(float) - Units of microseconds.
+> 5. 4 bytes - (long) - Number of triggers. This is not a member of the Event class, since it's simply the size of the trigger list. For each trigger the byte structure is given as :
+>     1. 8 bytes - trigger time (float) - Time of the trigger with respect to the start of the event.
+>     2. 4 bytes - nchar (int) - Number of characters in the trigger name.
+>     3. nchar * 1 byte - The characters that make up the trigger name.
+> 6. 8 bytes - nhits (long) - Number of hits in the event. The next set consists of nhits\*5\*8 bytes (one chunk of 8 bytes for each float of q,t,x,y,z).
+
+Please note that there is a typo. The number of triggers (long) takes 8 bytes, instead of 4.
+
+Read the [Demonstration](@ref) for more.
--- examples/demo.jl
+++ examples/demo.jl
@@ -1,4 +1,31 @@
-using Statistics, Plots, IceCubePublicDataReader
+# # Demonstration
+#
+# ## I/O
+#
+# This package provides basic I/O and print utility for Event.
+
+using IceCubePublicDataReader
+
+demo_file_path = joinpath(pkgdir(IceCubePublicDataReader),
+    "data",
+    "Level1_IC59_data_Run00115150_Part00000000_Event11.odf")
+
+event = open(demo_file_path) do io
+    read(io, Event)
+end
+
+# You can also `write` an Event out.
+
+buffer = IOBuffer()
+write(buffer, event)
+seekstart(buffer)
+retrieved_event = read(buffer, Event)
+
+# ## Plotting
+#
+# Here I replicated the event view shown [here](https://icecube.umd.edu/PublicData/tutorials/TheEventViewer.html).
+#
+# First I implemented the color mapping defined by the author.
 
 colormapping1(t) = RGB(cos(π/2 * t), sin(π/2 * t), 0)
 colormapping2(t) = RGB(0, cos(π/2 * t), sin(π/2 * t))
@@ -15,7 +42,14 @@ function colormapping(t)
     end
 end
 
-kw = (; markersize=5, grid=true, xlim=(-1000, 1000), ylim=(-1000, 1000), label=nothing)
+# Then I plotted the hits whose colors are determined by the time they are recorded. I skimmed off hits whose time is $0.8\sigma$ away from the mean since otherwise all the hits would have almost the same color.
+
+kw = (; markersize=5,
+      grid=true,
+      xlim=(-1000, 1000), ylim=(-1000, 1000),
+      label=nothing)
+
+using Statistics, Plots
 
 function myplot(event::Event)
     ts = [hit.t for hit in event.hits]
@@ -28,14 +62,13 @@ function myplot(event::Event)
     for hit in skim
         t = (hit.t - m) / s / 0.8 / 2 + 0.5
         c = colormapping(t)
-        scatter!(pxz, (hit.x, hit.z), markercolor=c, xlabel="x (m)", ylabel="z (m)"; kw...)
-        scatter!(pyz, (hit.y, hit.z), markercolor=c, xlabel="y (m)", ylabel="z (m)"; kw...)
+        scatter!(pxz, (hit.x, hit.z); kw...,
+                 markercolor=c, xlabel="x (m)", ylabel="z (m)")
+        scatter!(pyz, (hit.y, hit.z); kw...,
+                 markercolor=c, xlabel="y (m)", ylabel="z (m)")
     end
     plot(pxz, pyz)
 end
 
-open(joinpath(pkgdir(IceCubePublicDataReader), "data", "Level1_IC59_data_Run00115150_Part00000000_Event11.odf")) do f
-    event = read(f, Event)
-    myplot(event)
-end
+myplot(event)
 
